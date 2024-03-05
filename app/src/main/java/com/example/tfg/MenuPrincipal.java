d6c077c803f960cebc63f24424b54c43ec09147f
dmarta14 <diegomarta14@gmail.com>
2024-03-05 19:49:02 +0100

    Menus Principal y botones de abajo

--- app/src/main/java/com/example/tfg/MenuPrincipal.java
+++ app/src/main/java/com/example/tfg/MenuPrincipal.java
@@ -2,8 +2,6 @@ package com.example.tfg;
 
 import android.os.Bundle;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -11,6 +9,7 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.tfg.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MenuPrincipal extends AppCompatActivity {
 
