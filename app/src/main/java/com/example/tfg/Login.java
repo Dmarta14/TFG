c28e7d8cba0b85e834c1ba79db5f0364440ebac1
DIEGO <diegomarta14@gmail.com>
2025-03-07 00:52:03 +0100

    Login modificado

--- app/src/main/java/com/example/tfg/Login.java
+++ app/src/main/java/com/example/tfg/Login.java
@@ -23,90 +23,20 @@ import java.io.OutputStreamWriter;
 
 public class Login extends AppCompatActivity {
 
-    /*@Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
 
-        TextView mail = findViewById(R.id.UsuarioIni);
-        TextView contrasena1 = findViewById(R.id.PasswordIni);
-        TextView registrar = findViewById(R.id.Registrarse);
-
-
-        Button iniciarSesion = findViewById(R.id.Acceder);
-        iniciarSesion.setOnClickListener(v -> {
-            String email = mail.getText().toString();
-            String contra1 = contrasena1.getText().toString();
-            obtenerUsuario(email, contra1);
-        });
-
-        registrar.setOnClickListener(v -> {
-
-            Intent intent = new Intent(v.getContext(), Registro1.class);
-            startActivity(intent);
-
-        });
-    }
-
-
-    public void obtenerUsuario(String usuario, String contra) {
-        Data param = new Data.Builder()
-                .putString("param", "ExisteUsuarioContra")
-                .putString("mail", usuario)
-                .putString("contrasena", contra).build();
-        Log.d("Prueba inicio", "" + param);
-        OneTimeWorkRequest oneTimeWorkRequest =
-                new OneTimeWorkRequest.Builder(BD.class).setInputData(param).build();
-        WorkManager.getInstance(Login.this).enqueue(oneTimeWorkRequest);
-        WorkManager.getInstance(Login.this)
-                .getWorkInfoByIdLiveData(oneTimeWorkRequest.getId())
-                .observe(Login.this, workInfo -> {
-                    if (workInfo != null && workInfo.getState().isFinished()) {
-                        if (workInfo.getState() != WorkInfo.State.SUCCEEDED) {
-                            Toast.makeText(getApplicationContext(), "ERROR",
-                                    Toast.LENGTH_LONG).show();
-                        } else {
-                            Data d = workInfo.getOutputData();
-                            boolean b = d.getBoolean("existe", false);
-
-                            if (b) {
-                                Toast.makeText(getApplicationContext(), "existe un " +
-                                        "usuario", Toast.LENGTH_LONG).show();
-                                Intent intent = new Intent(getApplicationContext(),
-                                        MenuPrincipal.class);
-                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK
-                                        | Intent.FLAG_ACTIVITY_NEW_TASK);
-                                saveSession(usuario);
-                                startActivity(intent);
-                            } else {
-                                AlertDialog.Builder builder =
-                                        new AlertDialog.Builder(Login.this);
-                                builder.setTitle("Usuario o contraseña incorrectos");
-                                builder.setMessage("Introduce el usuario o contraseña " +
-                                        "correctamente o registrese en caso de no tener" +
-                                        " un usuario creado");
-                                builder.setPositiveButton("Volver", (dialogInterface,
-                                                                     i) -> {
-                                    Intent intent = new Intent(getApplicationContext(),
-                                            Login.class);
-                                    startActivity(intent);
-                                });
-                                AlertDialog alert = builder.create();
-                                alert.show();
-
-
-                            }
-                        }
-                    }
-                });
-    }*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_login);
         FirebaseFirestore db = FirebaseFirestore.getInstance ();
         Button acceder = findViewById(R.id.Acceder);
+        TextView registrar = findViewById(R.id.Registrarse);
+        registrar.setOnClickListener(v -> {
 
+            Intent intent = new Intent(v.getContext(), Registro1.class);
+            startActivity(intent);
+
+        });
         acceder.setOnClickListener ( v -> {
             String correo = ((EditText) findViewById(R.id.CorreoLogin)).getText().toString();
             String password = ((EditText) findViewById(R.id.PasswordLogin)).getText().toString();
