9bb7e2033d3ef27bebf13c4b92b98176bea9d9eb
DIEGO <diegomarta14@gmail.com>
2025-03-10 13:26:43 +0100

    FragmentEquipo

--- app/src/main/java/com/example/tfg/FragmentEquipo.java
+++ app/src/main/java/com/example/tfg/FragmentEquipo.java
@@ -1,64 +1,43 @@
 package com.example.tfg;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FragmentEquipo#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class FragmentEquipo extends Fragment {
+    private RecyclerView recyclerView;
+    private EquipoAdapter adapter;
+    private List<Equipo> equipoList;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public FragmentEquipo() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentEquipo.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FragmentEquipo newInstance(String param1, String param2) {
-        FragmentEquipo fragment = new FragmentEquipo();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    @SuppressLint("MissingInflatedId")
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_equipo, container, false);
+        recyclerView = view.findViewById(R.id.recyclerView1);
+        recyclerView.setLayoutManager(new LinearLayoutManager (getContext()));
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_equipo, container, false);
+        equipoList = new ArrayList<> ();
+        equipoList.add(new Equipo(R.string.anadirJugadores, R.drawable.anadir_jugador));
+        equipoList.add(new Equipo(R.string.gestionarJugadores, R.drawable.gestionar_equipo));
+
+        adapter = new EquipoAdapter(equipoList, requireContext ());
+        recyclerView.setAdapter(adapter);
+
+
+        return view;
     }
 }
